22c851a7195be11f693a7b34916a7204029da6dd
uoyo <344481481@qq.com>
2020-09-01 10:49:07 +0800

    更新docker compose

--- src/StepFly/Program.cs
+++ src/StepFly/Program.cs
@@ -17,7 +17,7 @@ namespace StepFly
                 .MinimumLevel.Override("System", LogEventLevel.Warning)
                 .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
                 .Enrich.FromLogContext()
-                .WriteTo.File(@"Logs\log.txt", rollingInterval: RollingInterval.Day)
+                .WriteTo.File(@"Logs\\log.txt", rollingInterval: RollingInterval.Day)
                 //.WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Code)
                 .CreateLogger();
 
